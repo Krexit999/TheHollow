79fe547181fa7e2715039836815bb6fce0aae9b6
Krexit <konradwashere@icloud.com>
2026-08-05 15:57:35 -0400

    Drop the throwaway probe

--- scripts/_probe.ts
+++
@@ -1,7 +0,0 @@
-import { ensureContentLoaded } from '../src/engine/content/index';
-ensureContentLoaded();
-import { MACHINE_DEMAND } from '../src/engine/systems/plant';
-import { allShells } from '../src/engine/shells';
-const k = Object.keys(MACHINE_DEMAND);
-console.log(k.length, k.join(' '));
-for (const s of allShells()) console.log(s.id, s.floorDepth);
